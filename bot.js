0a98686a4fa4ff07ca633bba3e03bc1ec01be7fb
Andy <nhm.nohalfmeasures@gmail.com>
2017-04-19 17:51:13 +0100

    Update, Token & Cleanup
    - the bot token is now stored in a json file
    - some code clean up and a message change

--- bot.js
+++ bot.js
@@ -2,11 +2,18 @@ var Discordie;
 try { Discordie = require("../"); } catch(e) {}
 try { Discordie = require("discordie"); } catch(e) {}
 
+try {
+	var Auth = require("./Auth.json");
+} catch (e){
+	console.log("Please create an auth.json like Auth.json.example with a bot token\n"+e.stack);
+	process.exit();
+}
+
 const Events = Discordie.Events;
 const bot = new Discordie({autoReconnect: true});
 
 bot.connect({
-  token: 'YOUR TOKEN HERE'
+  token: Auth.bot_token
 });
 
 bot.Dispatcher.on(Events.GATEWAY_READY, e => {
@@ -15,12 +22,12 @@ bot.Dispatcher.on(Events.GATEWAY_READY, e => {
 
 bot.Dispatcher.on(Events.MESSAGE_CREATE, e => {
   if (e.message.channel.isPrivate) {
-      console.log(`(PM) ${e.message.author.name}: ${e.message.content}`);
+      console.log(`(PM) ${e.message.author.username}: ${e.message.content}`);
   } else {
       console.log(`(${e.message.guild.name}:${e.message.channel.name}) ${e.message.author.username}: ${e.message.content}`);
   }
-  
-  if(e.message.content == 'PING'){
+
+  if(e.message.content == 'PING') {
     e.message.channel.sendMessage('PONG');
   }
 
@@ -33,6 +40,6 @@ bot.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 
   if (e.message.content == '[mentionme]') {
     var user = e.message.author;
-    e.message.channel.sendMessage(user.nickMention + ", example mention");
+    e.message.channel.sendMessage(user.nickMention + ", You happy now I've mentioned you?");
   }
 });
